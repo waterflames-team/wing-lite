a5d72fa0374909802e73076b48861511b86027df
折腾调 <1410007625@qq.com>
2020-12-20 09:30:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# lingkong wing lite 
+
+## 介绍
+灵翼是一个由[灵空团队](https://gitee.com/lingkonggzs) zhetengtiao
+开发，基于JavaScript的在线内容生成系统
+
+相比较于[lingkong-wing内核](https://gitee.com/lingkonggzs/lingkong-wing-kernel/tree/master)
+，它更快，生成速度更快，不用繁琐的上传html文件，只需修改json文件即可。
+
+## 亮点
+* 更快
+* 更强，支持markdown语法
+* 代码更加简洁明了
+* 无需更改、上传html文件，只需修改json文件
+* 更轻 主体+html总共50kb都不到
+* 对服务器要求更低，只需静态服务器即可
+
+## 使用
+1.到本仓库发行版界面下载winglite.js
+(注：exh版本为单页面版本，suc即双页面版本。详见demo)
+
+2.在本仓库内挑选主题
+（注：如果您下载的是exh版本，则需要到文件夹blog_exhibition选择主题。suc一样）
+
+3.新建一个html文件，内容像这样：
+```html
+<html>
+<head>
+    <title>xxx的博客</title>
+    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.js"></script>
+    <script src="blog_exhibition.js"></script><!-- 主题js -->
+    <script src="winglite_exh.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+</head>
+<body>
+<div id="winglite"></div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+```
+这里举例的是exh版本，html目录下需要放置刚刚下载的winglite_exh.js和下载的主题js文件
+body中间插入一个div，div id需要设置为winglite
+（注：先导入主题再导入winglite）
+
+4.在你下载的主题js文件里第一行编辑json存放地址
+（注：默认json文件在当前目录下）
+
+像这样：
+```js
+var jsonurl = './config.json';//json解析的url地址
+```
+修改jsonurl变量赋值的内容即可
+
+## demo
+
+
+## 开发者计划
+1. Fork 本仓库
+2. 新建 Feat_xxx 分支
+3. 提交代码
+4. 新建 Pull Request
---
+++ blog_exhibition/blog_exhibition.js
@@ -0,0 +1,60 @@
+var jsonurl = './config.json';//json解析的url地址
+
+function make_body_begin(author,headURL,introduce)
+{
+    //方法说明：make_body_begin方法，制作html的body开始语句
+    //参数说明：author作者，headURL头像地址，introduce个人介绍（全部必填）
+    //返回值说明：返回生成好的body开始语句
+    return '<div class=\"container-fluid\"><div class=\"row\"><nav class=\"col-md-2 d-none d-md-block bg-light sidebar\"><div class=\"sidebar-sticky\"></div></nav></div><div class=\"container-fluid\"><div class=\"row\"><nav class=\"col-md-2 d-none d-md-block bg-light sidebar\"><div class=\"sidebar-sticky\"></div></nav></div><div class=\"container-fluid\"><div class=\"row\"><nav class=\"col-md-2 d-none d-md-block bg-light sidebar\"><div class=\"sidebar-sticky\"></div></nav></div><!--head、small--><br><div class=\"alert alert-primary\" role=\"alert\"><h4 class=\"alert-heading\">'+ author +'的博客</h4><hr><p>欢迎来到'+ author +'的博客</p><p>这里就是博客的首页了</p></div><br><div class="alert alert-info" role="alert"><h4 class="alert-heading"><img src="'+ headURL +'" alt="" width="70px" class="rounded-circle">'+ author +'</h4><hr><p>'+ introduce +'</p></div><div class="container-fluid"><div class="row"><main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">';
+}
+
+function create_article(title,content,time,id)
+{
+    //方法说明：create_article方法，顾名思义创建一个文章显示区域
+    //参数说明：title标题，content内容，label标签，time时间，id文章唯一标识符（数字，字符串均可，保证唯一性）
+    //提示：title，contect，time，id参数必填，label选填（不填了话默认是“所有”标签）
+    //返回值说明：返回html语句，表示生成后的文章区域html
+    //提示：这个东西是循环使用的，即“有多少文章这个就调用多少次”
+    return '<div data-spy="scroll" data-target="#navbar-example3" data-offset="0"><h1 id="'+ id +'">'+ title +'</h1><h6 class="card-subtitle mb-2 text-muted">'+ time +'</h6><p>'+ content +'</p></div><br><br>';
+}
+
+function make_links_begin(title)
+{
+    //方法说明：make_links_begin方法，制作友情链接开始语句
+    //参数说明：title标题（全部必填）
+    //返回值说明：返回html语句
+    return '</main><nav class="col-md-2 d-none d-md-block bg-light sidebar"><div class="sidebar-sticky"><hr><br><ul class="nav flex-column"><p class="lead">'+ title +'</p>';
+}
+
+function make_links_end(URL,name)
+{
+    //方法说明：make_links_end方法，制作友情链接结束语句
+    //参数说明：URL链接，name显示标题（全部必填）
+    //返回值说明：返回生成好的友情链接结束语句
+    //提示：这个东西是循环使用的，即“有多少链接这个就调用多少次”
+    return '<li class="nav-item"><a class="nav-link" href="'+ URL +'"><span data-feather="home"></span>'+ name +'</a></li>';
+}
+
+function make_jump_links_begin()
+{
+    //方法说明：make_jump_links_begin方法，制作文章链接开始语句
+    //返回值说明：返回html语句
+    return '<br><hr><br><ul class="nav flex-column"><p class="lead">文章跳转</p><li class="nav-item">';
+}
+
+function make_jump_links_end(id,title)
+{
+    //方法说明：make_jump_links_end方法，制作文章链接结束语句
+    //参数说明：id文章唯一标识符，name显示标题（全部必填）
+    //返回值说明：返回生成好的文章链接结束语句
+    //提示：这个东西是循环使用的，即“有多少链接这个就调用多少次”
+    return '<a class="nav-link" href="#'+ id +'"><span data-feather="home"></span>'+ title +'</a></li>'
+}
+
+function make_body_end(author)
+{
+    //方法说明：make_body_end方法，制作html的body结束语句
+    //参数说明：author作者（全部必填）
+    //返回值说明：返回生成好的body结束语句
+    return '</ul></div></nav></div></div><br><hr><footer class="bd-footer text-muted"><div class="container-fluid p-3 p-md-5"><p>© 2020 '+ author +' | Make by wing lite<div class="d-flex align-items-center"><a href="#top" target="_self">返回顶部</a></div></p></div></footer><!--head、small--></div></div></div><!--head、small-->'
+}
